2a376aa54e2ad0ca952a19349ac5a5a465d60d22
Aaron Vasilev <oigen711@gmail.com>
2023-04-07 08:49:48 +0300

    Added server to solve dynamic PORT heroku issue

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv'
+import http from 'http'
 import pg, { Client } from 'pg'
 import { Telegraf } from 'telegraf'
 import { connectCommands } from './events/commands'
@@ -12,9 +13,9 @@ pg.types.setTypeParser(1082, (val) => val)
 
 const db = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
+  ssl: process.env.NODE_ENV === 'prod' ? {
     rejectUnauthorized: false
-  }
+  } : false
 })
 db.connect()
 
@@ -26,12 +27,12 @@ connectCommands(bot, db)
 connectHears(bot, db)
 connectOns(bot, db)
 
-const PORT = +process.env.PORT
 
-bot.launch(process.env.NODE_ENV === 'prod' && {
-  webhook: {
-    domain: process.env.DOMAIN,
-    port: PORT
-  }
-})
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.write('Hello World!')
+  res.end()
+}).listen(+process.env.PORT)
+
+bot.launch()
 console.log('Launch!')
