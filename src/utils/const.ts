046128325e264393bcdc9b72ed8242cbe005a2a2
Aaron Vasilev <oigen711@gmail.com>
2023-07-04 21:29:41 +0300

    Added prices

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -6,7 +6,7 @@ export enum Command {
   timetable = 'Timetable 🗓',
 }
 export enum Message {
-  contact = 'Address: <b>Haifa, Herzlia 16</b>\n\nTelephone: <b>0534257328</b> \n\nQuestions: @vialettochka',
+  contact = 'Address: <b>Haifa, Herzlia 16</b>\n\nTelephone: <b>0534257328</b> \n\nQuestions: @vialettochka\n\n<b>Prices:</b>\n 50₪ - first time\n 70₪ - visit without a pass\n<b>4 weeks passes:</b>\n 200₪ - 1 visit in a week\n 300₪ - 2 visits in a week\n 400₪ - unlimited visits',
   error = 'Oops, Something went wrong!',
   greeting = 'Hello to all my dear yoga students!\nI hope you are feeling healthy and happy.\nI look forward to practice together. See you on the mat🤍',
   register = 'See you in the session✨',
