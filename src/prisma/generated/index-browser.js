4931ec26cad1bc0974f478ea9341220aa4bd1528
Aron <oigen711@gmail.com>
2023-12-07 20:45:09 +0200

    Added extending for membership and prisma

---
+++ src/prisma/generated/index-browser.js
@@ -0,0 +1,201 @@
+
+Object.defineProperty(exports, "__esModule", { value: true });
+
+const {
+  Decimal,
+  objectEnumValues,
+  makeStrictEnum,
+  Public,
+  detectRuntime,
+} = require('./runtime/index-browser')
+
+
+const Prisma = {}
+
+exports.Prisma = Prisma
+exports.$Enums = {}
+
+/**
+ * Prisma Client JS version: 5.7.0
+ * Query Engine version: 79fb5193cf0a8fdbef536e4b4a159cad677ab1b9
+ */
+Prisma.prismaVersion = {
+  client: "5.7.0",
+  engine: "79fb5193cf0a8fdbef536e4b4a159cad677ab1b9"
+}
+
+Prisma.PrismaClientKnownRequestError = () => {
+  throw new Error(`PrismaClientKnownRequestError is unable to be run ${runtimeDescription}.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)};
+Prisma.PrismaClientUnknownRequestError = () => {
+  throw new Error(`PrismaClientUnknownRequestError is unable to be run ${runtimeDescription}.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.PrismaClientRustPanicError = () => {
+  throw new Error(`PrismaClientRustPanicError is unable to be run ${runtimeDescription}.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.PrismaClientInitializationError = () => {
+  throw new Error(`PrismaClientInitializationError is unable to be run ${runtimeDescription}.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.PrismaClientValidationError = () => {
+  throw new Error(`PrismaClientValidationError is unable to be run ${runtimeDescription}.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.NotFoundError = () => {
+  throw new Error(`NotFoundError is unable to be run ${runtimeDescription}.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.Decimal = Decimal
+
+/**
+ * Re-export of sql-template-tag
+ */
+Prisma.sql = () => {
+  throw new Error(`sqltag is unable to be run ${runtimeDescription}.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.empty = () => {
+  throw new Error(`empty is unable to be run ${runtimeDescription}.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.join = () => {
+  throw new Error(`join is unable to be run ${runtimeDescription}.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.raw = () => {
+  throw new Error(`raw is unable to be run ${runtimeDescription}.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.validator = Public.validator
+
+/**
+* Extensions
+*/
+Prisma.getExtensionContext = () => {
+  throw new Error(`Extensions.getExtensionContext is unable to be run ${runtimeDescription}.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.defineExtension = () => {
+  throw new Error(`Extensions.defineExtension is unable to be run ${runtimeDescription}.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+
+/**
+ * Shorthand utilities for JSON filtering
+ */
+Prisma.DbNull = objectEnumValues.instances.DbNull
+Prisma.JsonNull = objectEnumValues.instances.JsonNull
+Prisma.AnyNull = objectEnumValues.instances.AnyNull
+
+Prisma.NullTypes = {
+  DbNull: objectEnumValues.classes.DbNull,
+  JsonNull: objectEnumValues.classes.JsonNull,
+  AnyNull: objectEnumValues.classes.AnyNull
+}
+
+/**
+ * Enums
+ */
+
+exports.Prisma.TransactionIsolationLevel = makeStrictEnum({
+  ReadUncommitted: 'ReadUncommitted',
+  ReadCommitted: 'ReadCommitted',
+  RepeatableRead: 'RepeatableRead',
+  Serializable: 'Serializable'
+});
+
+exports.Prisma.LessonScalarFieldEnum = {
+  id: 'id',
+  date: 'date',
+  time: 'time',
+  description: 'description',
+  max: 'max'
+};
+
+exports.Prisma.MembershipScalarFieldEnum = {
+  user_id: 'user_id',
+  starts: 'starts',
+  ends: 'ends',
+  type: 'type',
+  lessons_avaliable: 'lessons_avaliable'
+};
+
+exports.Prisma.MigrationsScalarFieldEnum = {
+  id: 'id',
+  name: 'name',
+  run_on: 'run_on'
+};
+
+exports.Prisma.Registered_usersScalarFieldEnum = {
+  lesson_id: 'lesson_id',
+  registered: 'registered'
+};
+
+exports.Prisma.UserScalarFieldEnum = {
+  id: 'id',
+  username: 'username',
+  name: 'name',
+  emoji: 'emoji'
+};
+
+exports.Prisma.SortOrder = {
+  asc: 'asc',
+  desc: 'desc'
+};
+
+exports.Prisma.QueryMode = {
+  default: 'default',
+  insensitive: 'insensitive'
+};
+
+exports.Prisma.NullsOrder = {
+  first: 'first',
+  last: 'last'
+};
+
+
+exports.Prisma.ModelName = {
+  lesson: 'lesson',
+  Membership: 'Membership',
+  migrations: 'migrations',
+  registered_users: 'registered_users',
+  user: 'user'
+};
+
+/**
+ * This is a stub Prisma Client that will error at runtime if called.
+ */
+class PrismaClient {
+  constructor() {
+    return new Proxy(this, {
+      get(target, prop) {
+        const runtime = detectRuntime()
+        const edgeRuntimeName = {
+          'workerd': 'Cloudflare Workers',
+          'deno': 'Deno and Deno Deploy',
+          'netlify': 'Netlify Edge Functions',
+          'edge-light': 'Vercel Edge Functions',
+        }[runtime]
+
+        let message = 'PrismaClient is unable to run in '
+        if (edgeRuntimeName !== undefined) {
+          message += edgeRuntimeName + '. As an alternative, try Accelerate: https://pris.ly/d/accelerate.'
+        } else {
+          message += 'this browser environment, or has been bundled for the browser (running in `' + runtime + '`).'
+        }
+        
+        message += `
+If this is unexpected, please open an issue: https://github.com/prisma/prisma/issues`
+
+        throw new Error(message)
+      }
+    })
+  }
+}
+
+exports.PrismaClient = PrismaClient
+
+Object.assign(exports, Prisma)
